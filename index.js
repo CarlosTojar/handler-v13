dc9e3a0d098adb110c7c94b48a674d313ca3106a
Tojar <ctojarb@gmail.com>
2022-06-21 18:34:23 +0000

    Index.js

--- index.js
+++ index.js
@@ -60,11 +60,11 @@ client.on('messageCreate', async message => {
 
     let prefix = client.settings.get(message.guild.id, 'prefix');
 
-    if(!message.content.startsWith(prefix)&& message.content.startsWith(client.user.id)) message.reply(`My Prefix is: **\`${prefix}\`**, tpye \`${prefix}help\` for more information!`); //if the messages is not a command and someone tags the bot, then send an info msg
-    if (!message.content.startsWith(prefix)) return; //if the message does not starts with the prefix, return, so only commands are fired!
+    if(!message.content.startsWith(prefix)&& message.content.startsWith(client.user.id)) message.reply(`My Prefix is: **\`${prefix}\`**, tpye \`${prefix}help\` for more information!`);
+    if (!message.content.startsWith(prefix)) return; 
     
-    const args = message.content.slice(prefix.length).trim().split(/ +/g); //creating the argumest (each space == 1 arg)
-    const cmd = args.shift().toLowerCase(); //creating the cmd argument by shifting the args by 1
+    const args = message.content.slice(prefix.length).trim().split(/ +/g); 
+    const cmd = args.shift().toLowerCase(); 
     
     if(!message.content.startsWith(prefix) || cmd.length === 0) return;
     else{
@@ -72,39 +72,42 @@ client.on('messageCreate', async message => {
             message.react("")
         }catch{}
     }
-    if (message.content.includes(client.user.id)) { //if message contains musicium as a ping
-       return message.reply(new Discord.MessageEmbed().setColor("#ff712e").setAuthor(`${message.author.username}, My Prefix is ${prefix}, to get started; type ${prefix}help`, message.author.displayAvatarURL({ dynamic: true }), "https://su.musicium.eu"));
+    if (message.content.includes(client.user.id)) {
+       return message.reply(
+        new Discord.MessageEmbed()
+       .setColor("#ff712e")
+       .setAuthor(`${message.author.username}, My Prefix is ${prefix}, to get started; type ${prefix}help`, message.author.displayAvatarURL({ dynamic: true })));
     }
     
-    let command = client.commands.get(cmd); //get the command from the collection
-    if (!command) command = client.commands.get(client.aliases.get(cmd)); //if the command does not exist, try to get it by his alias
+    let command = client.commands.get(cmd); 
+    if (!command) command = client.commands.get(client.aliases.get(cmd)); 
 
    
-    if (command) //if the command is now valid
+    if (command) 
     {
-        if (!cooldowns.has(command.name)) { //if its not in the cooldown, set it too there
+        if (!cooldowns.has(command.name)) { 
             cooldowns.set(command.name, new Collection());
         }
         
-        const now = Date.now(); //get the current time
-        const timestamps = cooldowns.get(command.name); //get the timestamp of the last used commands
-        const cooldownAmount = (command.cooldown || 1) * 1000; //get the cooldownamount of the command, if there is no cooldown there will be automatically 1 sec cooldown, so you cannot spam it^^
+        const now = Date.now(); 
+        const timestamps = cooldowns.get(command.name); 
+        const cooldownAmount = (command.cooldown || 1) * 1000; 
       
-        if (timestamps.has(message.author.id)) { //if the user is on cooldown
-          const expirationTime = timestamps.get(message.author.id) + cooldownAmount; //get the amount of time he needs to wait until he can run the cmd again
+        if (timestamps.has(message.author.id)) { 
+          const expirationTime = timestamps.get(message.author.id) + cooldownAmount; 
       
-          if (now < expirationTime) { //if he is still on cooldonw
-            const timeLeft = (expirationTime - now) / 1000; //get the lefttime
+          if (now < expirationTime) { 
+            const timeLeft = (expirationTime - now) / 1000;
             return message.reply( 
               `Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
-            ); //send an information message
+            ); 
           }
         }
       
-        timestamps.set(message.author.id, now); //if he is not on cooldown, set it to the cooldown
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount); //set a timeout function with the cooldown, so it gets deleted later on again
+        timestamps.set(message.author.id, now); 
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount); 
       try{
-        command.run(client, message, args, message.author, args.join(" "), prefix); //run the command with the parameters:  client, message, args, user, text, prefix, 
+        command.run(client, message, args, message.author, args.join(" "), prefix); 
 
       }catch (error){
         console.log(error)
